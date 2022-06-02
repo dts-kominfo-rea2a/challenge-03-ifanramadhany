1c4690033f9983baaed2a1f07b887d4cb5911fd9
Ifan Ramadhany <ifanpersie14@gmail.com>
2022-06-02 15:26:50 +0700

    challenge-03-done/fix-01

--- index.js
+++ index.js
@@ -41,8 +41,7 @@ const totalBelanjaan = () => {
 
 // ! JANGAN DIMODIFIKASI
 const main = () => {
-  console.log("Selamat datang customer");
-
+  console.log("Selamat datang customer" + "\n");
   console.log("Belanjaan Anda adalah:");
   console.log(listBelanjaan?.(dataBelanjaan)?.join("\n"));
   console.log(
