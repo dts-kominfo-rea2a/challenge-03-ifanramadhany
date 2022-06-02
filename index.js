d5e096043197d85b686c6c4287b15ccc56bdcac7
Ifan Ramadhany <ifanpersie14@gmail.com>
2022-06-02 15:14:15 +0700

    challenge-03-done

--- index.js
+++ index.js
@@ -21,10 +21,23 @@ const dataBelanjaan = [
 ];
 
 // boleh dimodifikasi bila ingin menggunakan deklarasi fungsi yang normal
-const listBelanjaan = null;
+const listBelanjaan = () => {
+  let result = []
+  for (let i = 0; i < dataBelanjaan.length; i++) {
+    result.push(`- ${dataBelanjaan[i].nama} x ${dataBelanjaan[i].kuantitas}`)    
+  }
+  return result
+};
 
 // boleh dimodifikasi bila ingin menggunakan deklarasi fungsi yang normal
-const totalBelanjaan = null;
+const totalBelanjaan = () => {
+  let result = 0
+  for (let i = 0; i < dataBelanjaan.length; i++) {
+    result += dataBelanjaan[i].harga * dataBelanjaan[i].kuantitas
+  }
+  
+  return result
+};
 
 // ! JANGAN DIMODIFIKASI
 const main = () => {
